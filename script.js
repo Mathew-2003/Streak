102db58313148c0abe21cab1c2f38a56d9aa4d14
Mateo <matteoromero0320@gmail.com>
2026-04-06 11:30:36 -0300

    Mods a HMLT, CSS y JS. Ahora hay un modal para agregar y un iliminar todos

--- script.js
+++ script.js
@@ -1,37 +1,69 @@
-//Obtener elementos del HTML
-var boton = document.getElementById("agregar-habito");
-var tabla = document.getElementById("tabla-habitos");
+const table = document.getElementById('habit-table');
+const open = document.getElementById('open');
 
-//Escuchar el click
-boton.addEventListener("click", function() {
+const modalContainer = document.getElementById('modal_container');
+const input = document.getElementById('habit');
+const close = document.getElementById('close');
 
-    //Pedir nombre del hábito
-    var nombreHabito = prompt("Ingrese un nuevo Hábito: ");
+const deleteAll = document.getElementById('delete-all');
 
-    if (nombreHabito === null || nombreHabito.trim() === "") {
-        alert("No se ingreso nada. Intente de nuevo.");
+//Muestra el modal
+open.addEventListener('click', function() {
+    input.value = "";
+    modalContainer.classList.add('show');
+});
+
+
+//Agregar un hábito y cierra el modal
+close.addEventListener('click', function() {
+    let newRow = document.createElement("tr");
+  
+    let newHabit = input.value;
+    
+    if (newHabit === "") {
+        modalContainer.classList.remove('show');
         return;
     }
 
-    //Crear fila
-    var fila = document.createElement("tr");
-
-    //Crear celda del nombre
-    var celdaNombre = document.createElement("td");
-    celdaNombre.textContent = nombreHabito;
-    fila.appendChild(celdaNombre);
-
-    //Crear 7 cheackbox
-    for(var i = 0; i < 7; i++) {
-        var celda = document.createElement("td");
-
-        var checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-
-        celda.appendChild(checkbox);
-        fila.appendChild(celda);
+    //Verifica que no exista en la tabla
+    if (!uniqueHabit(newHabit)) {
+        alert('Ya existe.')
+        return
     }
+  
+    let cellName = document.createElement("td");
+    cellName.textContent = newHabit;
+    newRow.appendChild(cellName);
+  
+    for(let i = 0; i < 7; i++) {
+        let newCell = document.createElement("td");
     
-    //Agregar fila a la tabla
-    tabla.appendChild(fila);
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+    
+        newCell.appendChild(checkbox);
+        newRow.appendChild(newCell);
+    }
+  
+    table.appendChild(newRow);
+
+    modalContainer.classList.remove('show');
 });
+
+function uniqueHabit(newHabit) {
+    let rows = table.querySelectorAll('tr')
+
+    for (let row of rows) {
+        let firstCell =  row.children[0];
+
+        if (firstCell.textContent.toLocaleLowerCase() === newHabit.toLocaleLowerCase()) {
+            return false
+        }
+    }
+
+    return true
+}
+
+deleteAll.addEventListener('click', function() {
+    table.innerHTML = '';
+})
